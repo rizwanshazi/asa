211c75d94e3b491c71a88bf66319fa91ad346cda
rizwanshazi <rizwanmohmmad26@gmail.com>
2022-02-14 23:49:55 +0530

    init

---
+++ js/file.js
@@ -0,0 +1,42 @@
+
+//Q3
+var result=36;
+var total =null;
+
+switch(true){
+    case (result >=80):total=" succellyful passed";
+    console.log("succellyful passed:"+total);
+    break
+    case (result >=35):total= "passed";
+    console.log("succellyful passed:"+total);
+    break
+    case (result <35):total= "failuer";
+    console.log("failuer:"+total);
+}
+console.log(total);
+
+
+
+
+// q4 week
+
+var no =6;
+var daysinweek = null;
+
+switch(no){
+    case(1):daysinweek ="monday";
+    break;
+    case(2):daysinweek ="tuesday";
+    break;
+    case(3):daysinweek ="wenesday";
+    break;
+    case(4):daysinweek ="tharsday";
+    break;
+    case(5):daysinweek ="friday";
+    break;
+    case(6):daysinweek ="sturday";
+    break;
+    case(7):daysinweek ="sunday";
+    break;
+}
+console.log(daysinweek);
